8b70e97d272438091a15594604a70325a249b862
shyam kumar <38135327+shyamGITHUBJAVA@users.noreply.github.com>
2025-03-14 11:41:41 +0530

    first commit

---
+++ src/test/java/com/example/Springbootjenkins/SpringbootjenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Springbootjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootjenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
